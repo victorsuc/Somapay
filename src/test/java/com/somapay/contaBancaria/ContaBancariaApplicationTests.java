f0bb09f8f0787b6f71cf04d8475d59165f56117e
Victor Sucupira <victorsuc@hotmail.com>
2021-05-29 17:08:59 -0300

    Iniciando projeto com Spring Boot e migrations de criação de esquema e tabelas

---
+++ src/test/java/com/somapay/contaBancaria/ContaBancariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somapay.contaBancaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaBancariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
