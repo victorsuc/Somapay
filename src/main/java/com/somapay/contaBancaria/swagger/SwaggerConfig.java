18c191fc515af0eec340eb26fa2dab254e8e1627
Victor Sucupira <victorsuc@hotmail.com>
2021-05-31 19:55:44 -0300

    incluindo autorização no swagger

--- src/main/java/com/somapay/contaBancaria/swagger/SwaggerConfig.java
+++ src/main/java/com/somapay/contaBancaria/swagger/SwaggerConfig.java
@@ -1,42 +1,54 @@
 package com.somapay.contaBancaria.swagger;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+	private static final String headerString = "Authorization";
+
 	@Bean
-	public Docket detalheApi() {
- 
-		Docket docket = new Docket(DocumentationType.SWAGGER_2);
- 
-		docket
-		.select()
-		.apis(RequestHandlerSelectors.basePackage("com.somapay.contaBancaria"))
-		.paths(PathSelectors.any())
-		.build()
-		.apiInfo(this.informacoesApi().build());
- 
-		return docket;
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(informacoesApi())
+				.securityContexts(Arrays.asList(securityContext())).securitySchemes(Arrays.asList(apiKey())).select()
+				.apis(RequestHandlerSelectors.any()).paths(PathSelectors.any()).build();
 	}
- 
-	private ApiInfoBuilder informacoesApi() {
- 
-		ApiInfoBuilder apiInfoBuilder = new ApiInfoBuilder();
- 
-		apiInfoBuilder.title("Api-Somapay");
-		apiInfoBuilder.description("Api para teste da somapay");
-		apiInfoBuilder.version("1.0");
- 
-		return apiInfoBuilder;
- 
+
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", headerString, "header");
+	}
+
+	@SuppressWarnings("deprecation")
+	private ApiInfo informacoesApi() {
+
+		return new ApiInfo("API REST SOMAPAY",
+				"API de cadastro e consulta de empresa e funcionarios e pagamento de salários", null, null, null, null,
+				null);
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder().securityReferences(defaultAuth()).build();
+	}
+
+	List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+		authorizationScopes[0] = authorizationScope;
+		return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
 	}
 }
